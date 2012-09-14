670cd51ad1b985fb7beafdd03b4a51100b206e01
Tobias Adam <tow8ie@gmail.com>
2012-09-14 18:05:24 +0200

    Added my mininal Vim formula.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew-tow8ie
+
+This should become a collection of my personalized Homebrew formulas.
+
+## Installing
+
+    brew tap tow8ie/tow8ie
+    brew install <formula>
+
---
+++ vim-tow8ie.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class VimTow8ie < Formula
+  homepage 'http://www.vim.org/'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-661'
+  version '7.3.661'
+
+  head 'https://vim.googlecode.com/hg/'
+
+  def install
+    system "make", "clean"
+    system "./configure", "--disable-gui",
+                          "--with-mac-arch=i386",
+                          "--enable-multibyte",
+                          "--enable-fontset",
+                          "--with-compiledby='Tobias Adam <tow8ie@gmail.com>'",
+                          "--without-x",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
+
